b21e7d5baab50dc0ab430b5d31ead3d5efeb0a3f
jithumj <jithu@rawdatatech.com>
2026-04-01 18:34:38 +0530

    updated the project and adde scalar ui for api instead of swagger

--- IconnectDashboardAPIGateway/Program.cs
+++ IconnectDashboardAPIGateway/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,13 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseAuthorization();
