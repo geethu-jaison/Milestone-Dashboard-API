d303be328168e0425a06c15fe41a8e20644f08bc
jithumj <jithu@rawdatatech.com>
2026-04-02 16:31:58 +0530

    created 4 layer api structure with seperation of concerns with folder structure

---
+++ IconnectDashboardGateway.API/Controllers/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IconnectDashboardGateway.API.Controllers
+{
+    public class CameraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IconnectDashboardGateway.Application/Services/CameraService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IconnectDashboardGateway.Application.Interfaces;
+
+namespace IconnectDashboardGateway.Application.Services
+{
+    public class CameraService:ICameraService
+    {
+    }
+}
